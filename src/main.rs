f269c6b80903cef70476629985aa48940d0de7b8
Joshua Karns <jkarns275@gmail.com>
2018-01-31 22:00:23 -0500

    Mostly works, command line configurable now

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,33 @@ mod server;
 mod test;
 mod util;
 
+use test::*;
+
+use util::pretty_print;
+
 use std::env;
 
 const USAGE_MESSAGE: &'static str = r#"
-Usage: dl1 [mode]
+Usage: dl1 [mode] [tests]
 
-modes: serve, echo
+modes: serve, echo, help
+
+test format (keep quotes): "[UDP|TCP] [num_messages] [message_len]"
 
 "#;
 
 const ECHO: &'static str = "echo";
-const SERVE: &'static str = "serve";
+const TEST: &'static str = "test";
+const REQ_DATA: &'static str = "required";
 
 fn main() {
-    let args = env::args().into_iter().collect::<Vec<String>>();
+    let mut args = env::args().into_iter().collect::<Vec<String>>();
 
     if args.len() == 1 {
         println!("{}", USAGE_MESSAGE);
     } else if (&args[1] == ECHO) {
         server::Server::echo().unwrap();
-    } else if (&args[1] == SERVE) {
+    } else if (&args[1] == TEST) {
         let mut server;
         match server::Server::new() {
             Ok(s) => server = s,
@@ -31,16 +38,78 @@ fn main() {
             }
         };
 
-        let result = server.run_tests(
-            vec![
-                test::Test::UdpTest(test::TestSpec { num_messages: 8, message_len: 1024 }),
-                test::Test::UdpTest(test::TestSpec { num_messages: 8, message_len: 65504 }),
-            ]
-        );
+        if args.len() < 3 {
+            pretty_print("LOG", "Program Argument", &format!("No tests provided."), false);
+            return
+        }
 
-        println!("{:?}", result);
+        let mut tests: Vec<test::Test> = Vec::with_capacity(args.len() - 2);
+        for arg in &mut args[2..] {
+            arg.replace("\"", "");
+            let tokens: Vec<String> = arg.split(' ').map(|s| s.to_lowercase()).collect();
+            if tokens.len() < 3 {
+                pretty_print("ERR", "Program Argument", &format!("'{}' is not a valid test.", arg), false);
+                continue
+            }
+            let mut num_messages = 0;
+            if let Ok(val) = tokens[1].parse::<u32>() {
+                num_messages += val;
+            } else {
+                pretty_print("ERR", "Program Argument", &format!("'{}' is not a valid number.", tokens[1]), false);
+                continue
+            }
+
+            let mut message_len = 0;
+            if let Ok(val) = tokens[2].parse::<usize>() {
+                message_len += val
+            } else {
+                pretty_print("ERR", "Program Argument", &format!("'{}' is not a valid number.", tokens[2]), false);
+                continue
+            }
+
+            if &tokens[0] == "udp" {
+                tests.push(test::Test::UdpTest(test::TestSpec {
+                    num_messages,
+                    message_len,
+                }));
+            } else if &tokens[0] == "tcp" {
+                tests.push(test::Test::TcpTest(test::TestSpec {
+                    num_messages,
+                    message_len,
+                }));
+            } else {
+                pretty_print("ERR", "Program Argument", &format!("'{}' is not a valid connection type (tcp or udp only).", tokens[1]), false);
+            }
+        }
+
+        let result = server.run_tests(tests);
+
+        //println!("{:?}", result);
 
         println!("Successfully created server")
+    } else if &args[1] == REQ_DATA {
+        let mut server;
+        match server::Server::new() {
+            Ok(s) => server = s,
+            Err(e) => {
+                println!("Encountered error '{}' while trying to create server.", e);
+                return
+            }
+        };
+
+        server.run_tests(vec![
+            Test::TcpTest(TestSpec { message_len: 1, num_messages: 64 }),
+            Test::TcpTest(TestSpec { message_len: 64, num_messages: 64 }),
+            Test::TcpTest(TestSpec { message_len: 1024, num_messages: 64 }),
+            Test::UdpTest(TestSpec { message_len: 1, num_messages: 64 }),
+            Test::UdpTest(TestSpec { message_len: 64, num_messages: 64 }),
+            Test::UdpTest(TestSpec { message_len: 1024, num_messages: 64 }),
+            Test::TcpTest(TestSpec { message_len: 1024, num_messages: 64 }),
+            Test::TcpTest(TestSpec { message_len: 1024 * 16, num_messages: 64 }),
+            Test::TcpTest(TestSpec { message_len: 1024 * 64, num_messages: 64 }),
+            Test::TcpTest(TestSpec { message_len: 1024 * 256, num_messages: 64 }),
+            Test::TcpTest(TestSpec { message_len: 1024 * 1024, num_messages: 64 }),
+        ]);
     } else {
         println!("{}", USAGE_MESSAGE);
     }
